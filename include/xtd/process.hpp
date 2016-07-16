1fd8ef85374f915e0fb3b4dd400aaf7a3477b520
David Mott <mott.david.j@gmail.com>
2016-07-15 23:21:40 -0500

    work on process, and library enum

--- include/xtd/process.hpp
+++ include/xtd/process.hpp
@@ -10,16 +10,25 @@ namespace xtd{
 
   class process{
   public:
-  #if ((XTD_OS_MINGW | XTD_OS_WINDOWS) & XTD_OS)
-    using id_type = DWORD;
-  #endif
+
+#if (XTD_OS_LINUX & XTD_OS)
+    using pid_type = pid_t;
+#elif (XTD_OS_WINDOWS & XTD_OS)
+    using pid_type = HINSTANCE;
+#else
+#error "Unsupported system for xtd::process"
+#endif
+
+    static process& this_process(){
+      static process _this_process;
+      return _this_process(getpid());
+    }
+
+  private:
+    process(pid_type hPid) : _pid(hPid){}
+    pid_type _pid;
   };
 
 
-  namespace this_process{
-#if ((XTD_OS_MINGW | XTD_OS_WINDOWS) & XTD_OS)
-    process::id_type id(){ return GetCurrentProcessId(); }
-#endif
-  }
 
 }
