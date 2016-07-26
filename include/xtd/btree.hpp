667eed64415faa953de2a55b3c58812b1937fbd9
David Mott <mott.david.j@gmail.com>
2016-07-25 20:56:38 -0500

    add example_nlp.cpp btree.hpp

---
+++ include/xtd/btree.hpp
@@ -0,0 +1,20 @@
+/** @file
+STL-ish map using on disk b-tree
+@copyright David Mott (c) 2016. Distributed under the Boost Software License Version 1.0. See LICENSE.md or http://boost.org/LICENSE_1_0.txt for details.
+*/
+
+
+namespace xtd{
+  template <typename _KeyT, typename _ValueT> class btree{
+
+  public:
+    using key_type = _KeyT;
+    using value_type = _ValueT;
+
+    bool insert(const key_type& key, const value_type& value){
+
+    }
+
+
+  };
+}
--- include/xtd/nlp/nlp.hpp
+++ include/xtd/nlp/nlp.hpp
@@ -3,3 +3,8 @@ natural language processing
 @copyright David Mott (c) 2016. Distributed under the Boost Software License Version 1.0. See LICENSE.md or http://boost.org/LICENSE_1_0.txt for details.
 */
 
+namespace xtd{
+  namespace nlp{
+
+  }
+}
