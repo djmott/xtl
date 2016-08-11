7a2696fb4b99788ba9aa3f6168b5c74dab638d70
David Mott <mott.david.j@gmail.com>
2016-08-11 07:57:00 -0500

    disable btree example for now

--- examples/example_btree.cpp
+++ examples/example_btree.cpp
@@ -3,6 +3,9 @@
  * @copyright David Mott (c) 2016. Distributed under the Boost Software License Version 1.0. See LICENSE.md or http://boost.org/LICENSE_1_0.txt for details.
  */
 
+#if 1
+int main(){}
+#else
 #include <xtd/btree.hpp>
 #include <xtd/executable.hpp>
 
@@ -18,3 +21,4 @@ int main(){
   xtd::filesystem::remove(oFile);
   return 0;
 }
+#endif
