0b5114b742c4f257283dead5c92c4bf077c4c36b
David Mott <mott.david.j@gmail.com>
2016-08-19 22:51:08 -0500

    add a moby based POS tagger add pos enum based on wordnet and moby categories

--- include/xtd/nlp/nlp.hpp
+++ include/xtd/nlp/nlp.hpp
@@ -16,109 +16,106 @@ natural language processing
 #include <xtd/string.hpp>
 #include <xtd/var.hpp>
 #include <xtd/type_info.hpp>
+#include <xtd/wrapped_type.hpp>
+#include <xtd/debug.hpp>
 
 namespace xtd{
   namespace nlp{
 
-    class string_vector : public std::vector<xtd::string>{
-      using _super_t = std::vector<xtd::string>;
-    public:
-      template <typename ... _ArgTs> string_vector(_ArgTs&&...oArgs) : _super_t(std::forward<_ArgTs>(oArgs)...){}
-      template <typename _Ty> string_vector& operator=(_Ty&& rhs){
-        _super_t::operator=(std::forward<_Ty>(rhs));
-        return *this;
-      }
+
+    using raw_text = WRAPPED(xtd::string);
+    using lemma = WRAPPED(xtd::string);
+
+    enum part_of_speech : uint64_t {
+      unknown = 0,
+      adj_ppl = 1,
+      adj_pert = (adj_ppl << 1),
+      adj = (adj_ppl | adj_pert),
+      adv = (adj_pert << 1),
+      noun_Tops = (adv << 1),
+      noun_act = (noun_Tops << 1),
+      noun_animal = (noun_act << 1),
+      noun_artifact = (noun_animal << 1),
+      noun_attribute = (noun_artifact << 1),
+      noun_body = (noun_attribute << 1),
+      noun_cognition = (noun_body << 1),
+      noun_communication = (noun_cognition << 1),
+      noun_event = (noun_communication << 1),
+      noun_feeling = (noun_event << 1),
+      noun_food = (noun_feeling << 1),
+      noun_group = (noun_food << 1),
+      noun_location = (noun_group << 1),
+      noun_motive = (noun_location << 1),
+      noun_object = (noun_motive << 1),
+      noun_person = (noun_object << 1),
+      noun_phenomenon = (noun_person << 1),
+      noun_plant = (noun_phenomenon << 1),
+      noun_possession = (noun_plant << 1),
+      noun_process = (noun_possession << 1),
+      noun_quantity = (noun_process << 1),
+      noun_relation = (noun_quantity << 1),
+      noun_shape = (noun_relation << 1),
+      noun_state = (noun_shape << 1),
+      noun_substance = (noun_state << 1),
+      noun_time = (noun_substance << 1),
+      noun_plural = (noun_time << 1),
+      noun_phrase = (noun_plural << 1),
+      noun = (noun_Tops | noun_act | noun_animal | noun_artifact | noun_attribute | noun_body | noun_cognition |
+        noun_communication | noun_event | noun_feeling | noun_food | noun_group | noun_location | noun_motive |
+        noun_object | noun_person | noun_phenomenon | noun_plant | noun_possession | noun_process | noun_quantity |
+        noun_relation | noun_shape | noun_state | noun_substance | noun_time | noun_plural | noun_phrase),
+      verb_cognition = (noun_phrase << 1),
+      verb_communication = (verb_cognition << 1),
+      verb_competition = (verb_communication << 1),
+      verb_consumption = (verb_competition << 1),
+      verb_contact = (verb_consumption << 1),
+      verb_creation = (verb_contact << 1),
+      verb_emotion = (verb_creation << 1),
+      verb_motion = (verb_emotion << 1),
+      verb_perception = (verb_motion << 1),
+      verb_possession = (verb_perception << 1),
+      verb_social = (verb_possession << 1),
+      verb_stative = (verb_social << 1),
+      verb_weather = (verb_stative << 1),
+      verb_body = (verb_weather << 1),
+      verb_change = (verb_body << 1),
+      verb_participle = (verb_change << 1),
+      verb_transitive = (verb_participle << 1),
+      verb_intransitive = (verb_transitive << 1),
+      verb = (verb_cognition | verb_communication | verb_competition | verb_consumption | verb_contact |
+        verb_creation | verb_emotion | verb_motion | verb_perception | verb_possession | verb_social |
+        verb_stative | verb_weather | verb_body | verb_change | verb_participle | verb_transitive | verb_intransitive),
+      conjunction = (verb_intransitive << 1),
+      preposition = (conjunction << 1),
+      interjection = (preposition << 1),
+      definite_article = (interjection << 1),
+      indefinite_article = (definite_article << 1),
+      nominative = (indefinite_article << 1),
+      pronoun = (nominative << 1),
     };
-
-
-    namespace _ {
-      class noun;
-      class adjective;
-      class verb;
-      class adverb;
-      class pronoun;
-      class preposition;
-      class determiner;
-      class interjection;
-      class conjunction;
-    }
-
-
-    class lexical_category{
-    public:
-      using vector = std::vector<lexical_category>;
-      virtual ~lexical_category(){}
-      virtual const std::type_info& type() const { return typeid(lexical_category); }
-      virtual bool is_a(const std::type_info& oType) const { return type() == oType; }
-
-      using noun = _::noun;
-      using adjective = _::adjective;
-      using verb = _::verb;
-      using adverb = _::adverb;
-      using pronoun = _::pronoun;
-      using preposition = _::preposition;
-      using determiner = _::determiner;
-      using interjection = _::interjection;
-      using conjunction = _::conjunction;
-    };
-
-    namespace _{
-
-
-
-      template <typename _Ty> class lexical_category_impl : public lexical_category{
-      public:
-        virtual const std::type_info& type() const override { return typeid(_Ty); }
-        virtual bool is_a(const std::type_info& oType) const override { return (type() == oType); }
-        const std::string& value() const { return _value; }
-      protected:
-        std::string _value;
-      };
-
-
-      class noun : public lexical_category_impl<noun>{};
-      class adjective : public lexical_category_impl<adjective>{};
-      class verb : public lexical_category_impl<verb>{};
-      class adverb : public lexical_category_impl<adverb>{};
-      class pronoun : public lexical_category_impl<pronoun>{};
-      class preposition : public lexical_category_impl<preposition>{};
-      class determiner : public lexical_category_impl<determiner>{};
-      class interjection : public lexical_category_impl<interjection>{};
-      class conjunction : public lexical_category_impl<conjunction>{};
-
-      class sentence;
-      class noun_phrase;
-      class prepositional_phrase;
-      class verb_phrase;
-
-      template <typename _Ty> struct opt{
-        using type = _Ty;
-      };
-
-    }
-
-
-
-    struct BOW{
-      std::map<std::string, size_t> operator()(const std::vector<xtd::string>& oDoc) const{
-        std::map<std::string, size_t> oRet;
-        for (const auto& oItem : oDoc){
-          ++oRet[oItem];
-        }
-        return oRet;
-      }
-    };
-
-
-    struct SimpleParse{
-      std::vector<xtd::string> operator()(const std::string& src) const {
-        return xtd::string(src).split({'.', '!', '?', ' ', ','});
-      }
-    };
-
-
-
   }
+
+  template <> class _::xstring_format<char, const nlp::part_of_speech & > {
+  public:
+    inline static string format(const nlp::part_of_speech & iPOS) {
+      xtd::string sRet = "(";
+      if (nlp::part_of_speech::unknown == iPOS) sRet += "unknown";
+      if (nlp::part_of_speech::adj & iPOS) sRet += "adj | ";
+      if (nlp::part_of_speech::adv & iPOS) sRet += "adv | ";
+      if (nlp::part_of_speech::noun & iPOS) sRet += "noun | ";
+      if (nlp::part_of_speech::verb & iPOS) sRet += "verb | ";
+      if (nlp::part_of_speech::conjunction & iPOS) sRet += "conjunction | ";
+      if (nlp::part_of_speech::preposition & iPOS) sRet += "preposition | ";
+      if (nlp::part_of_speech::interjection & iPOS) sRet += "interjection | ";
+      if (nlp::part_of_speech::definite_article & iPOS) sRet += "definite_article | ";
+      if (nlp::part_of_speech::indefinite_article & iPOS) sRet += "indefinite_article | ";
+      if (nlp::part_of_speech::nominative & iPOS) sRet += "nominative | ";
+      if (nlp::part_of_speech::pronoun & iPOS) sRet += "pronoun | ";
+      sRet += ")";
+      return sRet;
+    }
+  };
+
 }
 
 
